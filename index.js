72433031c19898efcfdcc4c6ec50249b091ae271
Saad Najm <osamisan1@gmail.com>
2023-01-15 15:54:28 +0100

    added files

---
+++ index.js
@@ -0,0 +1,12 @@
+const copy=document.querySelector('.copy-me')
+copy.addEventListener('copy',()=>{
+console.log("hey dont copy")
+})
+const box=document.querySelector(".box")
+box.addEventListener('mousemove',(e)=>{
+console.log(e.offsetX, e.offsetY)
+box.textContent=`x pos -${e.offsetX} y pos- ${e.offsetY}`
+})
+document.addEventListener('wheel',e=>{
+    console.log(e.pageX ,e.pageY)
+})
